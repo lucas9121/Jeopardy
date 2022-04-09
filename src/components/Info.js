c8aeff05a18a7b711ad73734d25e0ca5cf3bac53
Lucas <lucas2carlos@gmail.com>
2022-04-09 16:17:40 -0400

    building Info

---
+++ src/components/Info.js
@@ -0,0 +1,19 @@
+export default function Info() {
+    return(
+        <main>
+            <div className="info-display">
+                <h2 className="h2"> Category: </h2>
+                <p>{trivia ? trivia.category.title : null} </p>
+            </div>
+            <div className="info-display">
+                <h2 className="h2"> Points: </h2>
+                <p>{trivia.value} </p>
+            </div>
+            <div className="info-display">
+                <h2 className="h2"> Answer: </h2>
+                <p>{trivia.question}</p>
+            </div>
+        </main>
+    )
+    
+}
