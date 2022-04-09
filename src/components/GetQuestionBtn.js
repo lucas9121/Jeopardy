878a11c6003d2d9d29515eaefb1b3f44d32705dd
Lucas <lucas2carlos@gmail.com>
2022-04-09 09:32:11 -0400

    building getQuestion button

--- src/components/GetQuestionBtn.js
+++ src/components/GetQuestionBtn.js
@@ -1,7 +1,7 @@
 export default function getQuestionBtn ({question}) {
     return (
         <div id="question-btn-div">
-            <h2>Let's Play!</h2>
+            <h2 className="h2">Let's Play!</h2>
             <button className="question-btn"
             onClick={(e) => {
                 question()
