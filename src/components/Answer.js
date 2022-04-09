e85eb748da81523b0f1b209027aaf6d5073137d1
Lucas <lucas2carlos@gmail.com>
2022-04-09 16:21:53 -0400

    building Answer

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Answer() {
+export default function Answer({trivia}) {
     const [answer, setAnswer] = useState("")
     return(
         <main>
