8cfae85052d8317006ed017271d08020c8bb0787
Lucas <lucas2carlos@gmail.com>
2022-04-09 16:16:01 -0400

    building Score

---
+++ src/components/Score.js
@@ -0,0 +1,14 @@
+export default function Score() {
+    return(
+        <main>
+            <div id="score">
+                <h2 className="h2">Score: <span id="score-number">{scoreNum}</span></h2>
+            </div>
+            <div id="buttons">
+                <button onClick={(e) => {setScoreNum(scoreNum - trivia.value)}}>Decrease</button>
+                <button onClick={(e) => {setScoreNum(scoreNum + trivia.value)}}>Increase</button>
+                <button onClick={(e) => {setScoreNum(0)}}>Reset</button>
+            </div>
+        </main>
+    )
+}
